878552ac86ad01da7ce308980bfbf8d10ab5113f
Sam Eddy <sam-kettle@outlook.com>
2020-07-21 20:28:38 +0100

    Add header & basic styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NewsList from "./components/NewsList";
+import Header from "./components/Header";
 import "./assets/app.css";
 
 const App = () => {
     return (
         <>
+            <Header />
             <NewsList />
         </>
     );
